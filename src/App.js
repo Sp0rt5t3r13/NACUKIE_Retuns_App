5fb16fe88da9dd7df611383be5ddbe98a1a271bf
Glenn Smith <glenn.iom.appleid@gmail.com>
2026-02-04 23:59:11 +0000

    Update Logo in Dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import AttendanceForm from './components/AttendanceForm';
-import churchLogo from './assets/logo.png';
+import churchLogo from './assets/logo.svg';
 import { 
   auth, 
   createUserWithEmailAndPassword, 
